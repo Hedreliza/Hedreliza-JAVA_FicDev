f77c232470f94a38cef2a5ea1df4d66eb98f1711
Hedreliza <131677791+Hedreliza@users.noreply.github.com>
2023-05-30 10:28:27 -0400

    JAVA1-Dia2

---
+++ Capitulodia2/src/capitulodia2/DesafioDia2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package capitulodia2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ficdev
+ */
+public class DesafioDia2 {
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+     
+           
+        System.out.print("Digite seu nome: ");
+        String nome = input.next();
+        
+        System.out.print("Digite sua idade: ");
+        int idade = input.nextInt();
+        
+        System.out.println("Seu nome é " + nome + " e você tem " + idade + " anos.");
+    
+    }
+}
